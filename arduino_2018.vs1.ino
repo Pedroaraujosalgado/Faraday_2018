1aec95461d391f719b3341c569297de4b7609597
Pedro <pedrosalgado@id.uff.br>
2018-11-10 16:33:37 -0200

    atualizacao da seatswitch e da footswitch

--- arduino_2018.vs1.ino
+++ arduino_2018.vs1.ino
@@ -10,21 +10,19 @@
 // Analogico
 const int pinAcel1    = A3;                                  
 const int pinAcel2    = A4;
-const int pinMotor    = A2;
 const int pinCorrente = A0;
 const int pinFreio    = A5;
 
 // Digital
 const int pinRTD = 5;                                             // Pino de entrada do sinal RTD
-const int pinFBair = 13;                                           // Pino de entrada do sinal do feedback AIR
+const int pinFBair = 13;                                          // Pino de entrada do sinal do feedback AIR
 const int pinSirene =12;                                          // Pino Sirene
-const int pinShtd = 11;                                           // Pino comando Relé de shutdown
-const int pinShtdE = 10;                                          // Pino de Entrada de sinal de shutdown Externo
-const int pinBrk = 13;                                            // Pino de entrada do sinal de Freio
+const int pinShtd = 11;                                           // Pino comando Relé de shutdown                                * 
 const int pinSeatswitch = 4;                                      // Pino de saida do Seatswitch 
 const int pinFootswitch = 2;                                      // Pino de saida do Footswitch
 const int pinLedRTD = 7;                                          // Pino da led RTD  
 const int pinLedShtd = 8;                                         // Pino led Shutdown
+const int pinMotor    = 3;                                        // Pino do sinal PWM do motor
 
 
 // Variáveis de input e output 
@@ -34,6 +32,7 @@ int motor   = 0;                                                  // variavel PW
 int shtdout = 0;                                                  // Variável de indicação de Shutdown externo
 int diferenca = 0;                                                // Variavel que ve a diferenca de Acel1 e Acel2
 int freio = 0;                                                    // Variavel freio
+int estadoBrk = 0;                                                // Variavel com valor do freio
 
 // Variáveis de limite
 
@@ -41,7 +40,6 @@ const int delaySirene = 1500; // Tempo em que a sirene fica ligado (milissegundo
 
 // Variáveis de estado
 boolean feedbackAIR = 0;            // estado do FeedBack AIR
-boolean estadoBrk = 0;              // estado do Freio
 boolean estadoRTD = 0;              // estado do RTD
 boolean shtdE = 0;                  // estado do sht Externo
 boolean shtd = 0;                   // Variável de controle do shutdown
@@ -53,9 +51,8 @@ void setup()
 {
   pinMode (pinRTD         ,INPUT);
   pinMode (pinFBair       ,INPUT);
-  pinMode (pinShtdE       ,INPUT);
-  pinMode (pinBrk         ,INPUT);
-
+  
+  
   pinMode (pinSirene         ,OUTPUT);
   pinMode (pinShtd           ,OUTPUT);
   pinMode (pinSeatswitch     ,OUTPUT);
@@ -73,10 +70,10 @@ void loop()
 
   if(feedbackAIR == HIGH)                           // Se feedbackAIR estiver ligado vai entrar no if
   {
-    estadoBrk = digitalRead(pinBrk);                // Lê se o freio esta acionado ou não
+    estadoBrk = analogRead(pinFreio);                  // Lê se o freio esta acionado ou não
     digitalWrite(pinSeatswitch, HIGH);              // fecha a seatswitch
     estadoRTD = digitalRead(pinRTD);                // Lê estado do botão RTD
-    if(estadoRTD == HIGH)                           // se o pino RTD estiver 
+    if(estadoRTD == HIGH && estadoBrk > 100)                            // se o pino RTD estiver ligado
     {
       digitalWrite(pinFootswitch, HIGH);            // fecha o footswitch
       digitalWrite(pinSirene, HIGH);                // Liga a Sirene
@@ -93,24 +90,16 @@ void loop()
        motor = (acel1+acel2)/2;                            // pega a media de acel1 e acel2 
        analogWrite (pinMotor,motor);
        diferenca = (acel1 - acel2)*(acel1 - acel2);
-       shtdE = digitalRead(pinShtdE);
        freio = analogRead(pinFreio);
        //========================================================================================================================================================
        // PLAUSIBILIDADE
        
-       if(shtdE)                                      // Se shtdE for LOW (1) vai entrar no if
-       {
-        shtd = 1;
-        shtdout = 1;
-        digitalWrite(pinShtd, shtd);
-        digitalWrite(pinLedShtd, shtd);
-        break;
-       }
+     
        if(diferenca >= 100)                            // Se Acel1 e Acel2 tiverem uma diferença de 10 
        {
-        shtd = 1;
+        shtd = shtd + 1;
         shtdout = 1;
-        digitalWrite(pinShtd, shtd);
+        digitalWrite(pinSeatswitch, LOW);
         digitalWrite(pinLedShtd, shtd);
         break;                   
        }
@@ -118,7 +107,7 @@ void loop()
        {
         shtd = 1;
         shtdout = 1;
-        digitalWrite(pinShtd, shtd);
+        digitalWrite(pinSeatswitch, LOW);
         digitalWrite(pinLedShtd, shtd);
         break;
        }
@@ -126,7 +115,7 @@ void loop()
        {
         shtd = 1;
         shtdout = 1;
-        digitalWrite(pinShtd, shtd);
+        digitalWrite(pinSeatswitch, LOW);
         digitalWrite(pinLedShtd, shtd);
         break;
        }
@@ -134,18 +123,23 @@ void loop()
        {
         shtd = 1;
         shtdout = 1;
-        digitalWrite(pinShtd, shtd);
+        digitalWrite(pinSeatswitch, LOW);
         digitalWrite(pinLedShtd, shtd);
         break;
        }
       
        if(feedbackAIR) //Se feedbackAIR estiver desligado durante a aceleracao
        {
+        shtd=1;
+        shtdout = 1;
+        digitalWrite(pinSeatswitch, LOW);
+        digitalWrite(pinLedShtd, shtd);
         break;
        }
        //==========================================================================================================================================================
        
       }
+      
         analogWrite(pinMotor, 0);                         // Envia 0 de PWM pro motor 
               
       }
@@ -153,7 +147,6 @@ void loop()
   }else{
         digitalWrite(pinSeatswitch, LOW);            // abre a seatswitch
         digitalWrite(pinFootswitch, LOW);            // abre o footswitch 
-        digitalWrite(pinShtd,       LOW);
         digitalWrite(pinLedShtd,    LOW);    
        }
   
